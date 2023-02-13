532b824c842386eaffee5d864331a25fda290bdc
kaliloulah1155 <ibrahim.konate@ngser.com>
2023-02-13 09:02:58 +0000

    my first init

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\SongController;
 
 
 Route::post('register',[AuthController::class,'register']);
@@ -15,8 +16,9 @@ Route::middleware('auth:sanctum')->group(function(){
      Route::post('logout',[AuthController::class,'logout']);
      Route::get('users/{id}',[UserController::class,'show']);
      Route::put('users/{id}',[UserController::class,'update']);
-     Route::get('inside-mware',function(){
-        return response()->json('success',200);
-     });
+
+     Route::post('songs',[SongController::class,'store']);
+     Route::delete('songs/{id}/{user_id}',[SongController::class,'destroy']);
+      
 
 });
